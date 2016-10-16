58b4ebec93ed41835bff4a492797a1dfa3167395
Ruan de Menezes Costa <ruan.cocito@gmail.com>
2016-10-16 15:28:03 -0200

    new smartsampa.jar which contains the gtfs.zip in the resources folder

--- src/main/java/com/ws/Application.java
+++ src/main/java/com/ws/Application.java
@@ -13,7 +13,6 @@ import com.smartsampa.busapi.Trip;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
 import java.io.IOException;
 
 /**
